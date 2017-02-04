92c7f42b9a65f28668b610d10beb9bea694b4267
Akiz <shawpan.du@gmail.com>
2017-02-04 23:39:42 +0600

    feat: Add doc generator
    Implement documentation images generator. It saves the output of each step to present as documentation.

--- find_lane_lines.py
+++ find_lane_lines.py
@@ -55,7 +55,26 @@ def find_lane_lines(type):
         print('Invalid type requested')
 
 def doc():
-    pass
+    """ Execute and generate debug images
+    """
+    src = np.float32([[240,719],[579,450],[712,450],[1165,719]])
+    dst =  np.float32([[300,719],[300,0],[900,0],[900,719]])
+    transformer = PerspectiveTransformer(src, dst)
+    image = cv2.imread('test_images/test4.jpg')
+    cv2.imwrite('doc/distorted.jpg', image)
+    image = cv2.imread('doc/distorted.jpg')
+    undistort_image = undistort(image, objpoints, imgpoints)
+    cv2.imwrite('doc/undistorted.jpg', undistort_image)
+    processed_image = process_binary(undistort_image)
+    cv2.imwrite('doc/binary.jpg', processed_image*255)
+    processed_image = transformer.transform(processed_image)
+    cv2.imwrite('doc/birdeyeview.jpg', processed_image*255)
+    left_fit, right_fit, yvals, out_img = find_lanes(processed_image)
+    cv2.imwrite('doc/lanes.jpg', out_img)
+    processed_image = fit_lane(processed_image, undistort_image, yvals, left_fit, right_fit, transformer)
+    left_curvature, right_curvature, distance = get_curvature(left_fit, right_fit, yvals)
+    processed_image = draw_stat(processed_image, left_curvature, right_curvature, distance)
+    cv2.imwrite('doc/final.jpg', out_img)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()

9bc26d0fb24acfbdbd9bbe83a365bd075f56ecc1
Akiz <shawpan.du@gmail.com>
2017-02-08 13:20:48 +0600

    fix: Fix 'transformer' is not defined
    Fixed bug of undefined persepective transformer

--- lane_detector.py
+++ lane_detector.py
@@ -22,15 +22,12 @@ class LaneDetector:
             img: camera image
         """
         objpoints, imgpoints = self.calibration.get_calibration_points()
-        src = np.float32([[240,719],[579,450],[712,450],[1165,719]])
-        dst =  np.float32([[300,719],[300,0],[900,0],[900,719]])
-        transformer = PerspectiveTransformer(src, dst)
 
         undistort_image = undistort(img, objpoints, imgpoints)
         processed_image = process_binary(undistort_image)
         processed_image = self.transformer.transform(processed_image)
         left_fit, right_fit, yvals, out_img = find_lanes(processed_image)
-        processed_image = fit_lane(processed_image, undistort_image, yvals, left_fit, right_fit, transformer)
+        processed_image = fit_lane(processed_image, undistort_image, yvals, left_fit, right_fit, self.transformer)
         left_curvature, right_curvature, distance = get_measurements(left_fit, right_fit, yvals)
         processed_image = draw_stat(processed_image, left_curvature, right_curvature, distance)
 
